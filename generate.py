58f64be04d73370a70a24c755bdd019beaa7f3ab
Anton <a67770258@gmail.com>
2026-03-12 17:50:36 +0300

    исправления

--- generate.py
+++ generate.py
@@ -1,20 +1,9 @@
 import requests
 
-with open("sources.json", "r", encoding="utf-8") as f:
-    sources = [line.strip() for line in f if line.strip() and not line.startswith("#")]
-
-
-#raw_url = "https://github.com/igareck/vpn-configs-for-russia/raw/main/WHITE-SNI-RU-all.txt"
-
 headers = {'User-Agent': 'Mozilla/5.0'}
 
 source = ["https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/main/WHITE-SNI-RU-all.txt", 
           "https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/main/WHITE-CIDR-RU-checked.txt"]
-
-#for line in source:
-#    response = requests.get(line, headers=headers)
-#    response.encoding = 'utf-8'
-#    print("успех")
 
 all_url = []
 
@@ -27,12 +16,6 @@ for url in source:
     for text_line in response.text.splitlines():
         if text_line.startswith(('vless://', 'trojan://')):
             all_url.append(text_line)
-
-# Извлекаем строки, начинающиеся с vless:// или trojan://
-#links = []
-#for line in response.text.splitlines():
-#    if line.startswith(('vless://', 'trojan://')):
-#        links.append(line)
 
 print(f"Найдено ссылок: {len(all_url)}")
 for line in all_url:
